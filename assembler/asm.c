d106bf630fdc67c7ba35df28435f98958b0707bf
Alexandra Jon hollard <ajon-hol@il-h3.21-school.ru>
2019-06-07 21:02:02 +0300

    rofloparser

--- assembler/asm.c
+++ assembler/asm.c
@@ -6,7 +6,7 @@
 /*   By: ajon-hol <ajon-hol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 16:41:52 by ajon-hol          #+#    #+#             */
-/*   Updated: 2019/06/04 12:08:36 by ajon-hol         ###   ########.fr       */
+/*   Updated: 2019/06/07 20:42:48 by ajon-hol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,20 @@
 
 int	main(int argc, char **argv)
 {
-	t_unit *unit;
+	t_unit	*unit;
+	char	*rd;
 
 	if (argc < 2)
 		ft_printf("Usage: ./asm [possible flags] <sourcefile.s>\n");
-	else if ( /*readed = */read_s(argv[argc - 1]))
+	else if ((rd = read_s(argv[argc - 1])))
 	{
-		unit = initchamp(/*readed*/);
-		writechamp(unit, argv[argc - 1]);
-		free(unit);
+		if (parse(rd))
+		{
+			unit = initchamp(/*readed*/);
+			writechamp(unit, argv[argc - 1]);
+			free(unit);
+		}
+		free(rd);
 	}
 	return (0);
 }
