101b47d39c5c30ec246c57fa844d9b029aa37074
Alexandra Jon hollard <ajon-hol@at-m4.21-school.ru>
2019-07-05 16:13:25 +0300

    vkusniatina

--- assembler/check_command.c
+++ assembler/check_command.c
@@ -6,7 +6,7 @@
 /*   By: ajon-hol <ajon-hol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 18:10:39 by ajon-hol          #+#    #+#             */
-/*   Updated: 2019/07/04 20:55:19 by ajon-hol         ###   ########.fr       */
+/*   Updated: 2019/07/05 16:03:24 by ajon-hol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,18 @@
 
 void	check_command(t_list **lst)
 {
-	static int	counter = 0;
-
-	if (ft_strequ(TOKEN, NAME_CMD_STRING) && !(counter & 1))
+	if (ft_strequ(TOKEN, NAME_CMD_STRING) && !(g_counter & 1))
 	{
-		counter |= 1;
+		g_counter |= 1;
 		*lst = (*lst)->next;
 		if (!(*lst) || (*lst && TTYPE != STRING))
 			c_error(lst, SYNTAX);
 		if (ft_strlen(TOKEN) > PROG_NAME_LENGTH)
 			c_error(lst, NAMELEN);
 	}
-	else if (ft_strequ(TOKEN, COMMENT_CMD_STRING) && !(counter & 2))
+	else if (ft_strequ(TOKEN, COMMENT_CMD_STRING) && !(g_counter & 2))
 	{
-		counter |= 2;
+		g_counter |= 2;
 		*lst = (*lst)->next;
 		if (!(*lst) || (*lst && TTYPE != STRING))
 			c_error(lst, SYNTAX);
